2633dd6e0e57e2cfe0cab4e476f1038ac5000c02
Leonardo Silveira <sombriks@gmail.com>
2023-07-15 16:08:00 -0300

    testing

--- src/main/java/sample/msk/App.java
+++ src/main/java/sample/msk/App.java
@@ -2,8 +2,9 @@ package sample.msk;
 
 import io.javalin.Javalin;
 import io.javalin.apibuilder.ApiBuilder;
-import org.apache.kafka.clients.KafkaClient;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,13 +20,24 @@ public class App {
     public static void main(String[] args) throws Exception {
         Properties props = new Properties();
         props.load(App.class.getResourceAsStream("/client.properties"));
-        KafkaProducer<String, Object> producer = new KafkaProducer<>(props);
-
-        Javalin
-                .create()
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        Javalin.create()
                 .routes(() -> {
-                    ApiBuilder.get("/list", ctx -> {
-                        ctx.json(producer.metrics());
+                    ApiBuilder.get("/send", ctx -> {
+                        producer.send(
+                                new ProducerRecord<>(
+                                        "teste",
+                                        ctx.queryParam("key"),
+                                        ctx.queryParam("value")),
+
+                                (recordMetadata, exception) -> {
+                                    LOG.info("{}", recordMetadata);
+                                    if (exception != null)
+                                        LOG.warn("problema: ", exception);
+                                    ctx.json("DONE!");
+                                }
+                        );
                     });
                 })
                 .start(7070);
